2f36cacf0b4ac88d7117c2cf9f41388b60ebe2d3
Sean Murphy <sean@iamseanmurphy.com>
2025-02-12 19:16:59 +0000

    2.7.0

--- publish-to-apple-news/build/adminSettings.asset.php
+++ publish-to-apple-news/build/adminSettings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '0e497502992e71e7aeb4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'b3d98c31ca9abd686e18');
--- publish-to-apple-news/build/pluginSidebar.asset.php
+++ publish-to-apple-news/build/pluginSidebar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins'), 'version' => '645db1bbe3bf6d798ea9');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '87d5b79979f2524517be');
